38d1a866c84763c2918044a5994d0931d518597a
Minnu <110650599+minnukota381@users.noreply.github.com>
2023-10-25 10:34:38 +0530

    Create 2. QueueArrayPushDisplay.c

---
+++ Data Structures and Algorithms using C/31. Queue using Array/2. QueueArrayPushDisplay.c
@@ -0,0 +1,31 @@
+//Declare Queue Variables
+#define MAX 10
+int queue[MAX];
+int rear=-1, front=-1;
+//This function should insert the x into the Queue. if success print "Successfully inserted." else "Queue is overflow."
+void enqueue(int x) {
+    if(rear==MAX-1){
+        printf("Queue is overflow.\n");
+    }
+    else {
+        rear=rear+1;
+        queue[rear]=x;
+        printf("Successfully inserted.\n");
+        if(front==-1){
+            front=0;
+        }
+    }
+}
+//This displays all the elements of the queue.
+//If the queue is empty display "Queue is empty."
+void display() {
+    if(front==1&&rear==-1){
+        printf("Queue is empty.\n");
+    } else {
+        printf("Elements in the queue : ");
+        for(int i=front;i<=rear;i++){
+            printf("%d ",queue[i]);
+        }
+        printf("\n");
+    }
+}

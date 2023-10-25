7765892a5c4061ac2628d8331ba00bbffea9c490
Minnu <110650599+minnukota381@users.noreply.github.com>
2023-10-25 10:39:06 +0530

    3. QueueArrayDequeueSize.c

---
+++ Data Structures and Algorithms using C/31. Queue using Array/3. QueueArrayDequeueSize.c
@@ -0,0 +1,53 @@
+#define MAX 10
+int queue[MAX];
+int front = -1, rear = -1;
+void dequeue() {
+    if(front==-1){
+        printf("Queue is underflow.\n");
+    } else {
+        printf("Deleted element = %d\n",queue[front]);
+        if(rear==front){
+            front=-1;
+            rear=-1;
+        } else {
+            front++;
+        }
+    }
+}
+void size() {
+    if(front==-1 && rear==-1){
+        printf("Queue size : 0\n");
+    } else {
+        printf("Queue size : %d\n",rear-front+1);
+    }
+}
+void isEmpty() {
+    if(front==-1 && rear==-1){
+        printf("Queue is empty.\n");
+    } else {
+        printf("Queue is not empty.\n");
+    }
+}
+void enqueue(int x) {
+    if (rear == MAX - 1) {
+        printf("Queue is overflow.\n");
+    } else {
+        rear++;
+        queue[rear] = x;
+        printf("Successfully inserted.\n");
+    }
+    if (front == -1) {
+        front++;
+    }
+}
+void display() {
+    if (front == -1 && rear == -1) {
+        printf("Queue is empty.\n");
+    } else {
+        printf("Elements in the queue : ");
+        for (int i = front; i <= rear; i++) {
+            printf("%d ",queue[i]);
+        }
+        printf("\n");
+    }
+}

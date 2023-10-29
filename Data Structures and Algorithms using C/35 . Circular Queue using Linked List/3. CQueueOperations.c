e1fb315fd4d93db10f83f86574896dad22d088b8
Minnu <110650599+minnukota381@users.noreply.github.com>
2023-10-29 23:23:01 +0530

    3. CQueueOperations.c

---
+++ Data Structures and Algorithms using C/35 . Circular Queue using Linked List/3. CQueueOperations.c
@@ -0,0 +1,70 @@
+struct queue {
+    int data;
+    struct queue *next;
+};
+typedef struct queue *CircularQueue;
+CircularQueue front=NULL,rear=NULL;
+void enqueue(int element){
+    CircularQueue temp=(CircularQueue)malloc(sizeof(struct queue));
+    if(temp==NULL){
+        printf("Circular queue is overflow.\n");
+    } else {
+        temp->data=element;
+        temp->next=NULL;
+        if(front==NULL){
+            front=temp;
+        } else {
+            rear->next=temp;
+        }
+        rear=temp;
+        rear->next=front;
+        printf("Successfully inserted.\n");
+    }
+}
+void dequeue(){
+    if(front==NULL){
+        printf("Circular queue is underflow.\n");
+        return;
+    }
+    CircularQueue temp=front;
+    if(front==rear){
+        front=NULL;
+        rear=NULL;
+    } else {
+        front=front->next;
+        rear->next=front;
+    }
+    printf("Deleted value = %d\n",temp->data);
+    free(temp);
+}
+void display(){
+    if(front==NULL){
+        printf("Circular queue is empty.\n");
+    } else {
+        CircularQueue temp = front;
+        printf("Elements in the circular queue : ");
+        do{
+            printf("%d ",temp->data);
+            temp=temp->next;
+        } while(temp!=front);
+        printf("\n");
+    }
+}
+void size(){
+    int count=0;
+    CircularQueue temp= front;
+    if(front!=NULL){
+        do{
+            count++;
+            temp=temp->next;
+        } while(temp!=front);
+    }
+    printf("Circular queue size : %d\n",count);
+}
+void isEmpty(){
+    if(front==NULL){
+        printf("Circular queue is empty.\n");
+    } else {
+        printf("Circular queue is not empty.\n");
+    }
+}
